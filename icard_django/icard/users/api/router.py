fe0ebe5f29eb8824ba001f6b4c9a1ec02100c5c5
alexdjdjx <117446274+alexdjdjx@users.noreply.github.com>
2023-11-07 07:30:48 +0100

    si

---
+++ icard_django/icard/users/api/router.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from users.api.views import UserApiViewSet, UserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router_user = DefaultRouter()
+
+router_user.register(
+    prefix='users', basename='users', viewset=UserApiViewSet
+)
+
+urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/me/', UserView.as_view())
+]
+
+urlpatterns += router_user.urls
